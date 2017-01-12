d3f083a3e0ee48bb055c09a32ae83cfbcd382853
kevin rei bento <k.rei-bento@groupe-grad.com>
2017-01-12 01:01:00 +0100

    oui

--- web/myCv/gulpfile.js
+++ web/myCv/gulpfile.js
@@ -95,5 +95,4 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
-    gulp.watch('css/**/*.css', browserSync.reload);
 });
